09198535fa7d6802785981a750fe51815d8a9ecc
Sergey Ohryamkin <sergey.ohryamkin@softservice.org>
2025-12-26 12:33:09 +0700

    Use history mode for Vue router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: '/login',
